2c38fbe567d698386b3b7702753af0b71feb5fd3
Erick Martin Meza Alonso <erickmartin.mezaalonso200245@gmail.com>
2023-05-04 20:26:47 -0600

    Cash Register bugged but working sometimes

--- CashRegister/index.js
+++ CashRegister/index.js
@@ -1,7 +1,7 @@
 function checkCashRegister(price, cash, cid) {
     let diff=cash - price;
     const submit = {
-        status:'close',
+        status:'CLOSED',
         change:[...cid]
     }
     const currencyAvalabilityArray = [
@@ -22,93 +22,97 @@ function checkCashRegister(price, cash, cid) {
         }
         flag++;
     }
-    console.log(currencyAvalabilityArray)
-    console.log(diff);
-/*     while(diff>0){
+    console.log(diff)
+    while(diff>0){
         if(diff>=100 && currencyAvalabilityArray[8][1]){
             if(change[8][1]>diff){
-                submit.change[8][1] = submit.change[8][1] - 100;
-                submit.status = 'open';
+                submit.status = 'OPEN';
             }else if(change[8][1]===diff){
-                submit.change[8][1] = submit.change[8][1] - 100;
-                submit.status = 'close';
+                submit.status = 'CLOSED';
                 currencyAvalabilityArray[8][1]=false;
             }
-            }else if(diff>=20 && currencyAvalabilityArray[7][1]){
+            submit.change[8][1] = submit.change[8][1] - 100;
+            diff = diff - 100;
+        }else if(diff>=20 && currencyAvalabilityArray[7][1]){
             if(change[7][1]>diff){
-                submit.change[7][1] = submit.change[7][1] - 20;
-                submit.status = 'open';
+                submit.status = 'OPEN';
                 }else if(change[7][1]===diff){
-                    submit.change[7][1] = submit.change[7][1] - 20;
-                    submit.status = 'close';
+                    submit.status = 'CLOSED';
                     currencyAvalabilityArray[7][1]=false;
                 }
-            }else if(diff>=10 && currencyAvalabilityArray[6][1]){
+                submit.change[7][1] = submit.change[7][1] - 20;
+                diff = diff - 20;
+        }else if(diff>=10 && currencyAvalabilityArray[6][1]){
             if(change[6][1]>diff){
-                submit.change[6][1] = submit.change[6][1] - 10;
-                submit.status = 'open';
+                submit.status = 'OPEN';
                 }else if(change[6][1]===diff){
-                submit.change[6][1] = submit.change[6][1] - 10;
-                    submit.status = 'close';
+                    submit.status = 'CLOSED';
                     currencyAvalabilityArray[6][1]=false;
                 }
-            }else if(diff>=5 && currencyAvalabilityArray[5][1]){
+                submit.change[6][1] = submit.change[6][1] - 10;
+                diff = diff - 10;
+        }else if(diff>=5 && currencyAvalabilityArray[5][1]){
             if(submit.change[5][1]>diff){
-                submit.change[5][1] = submit.change[5][1] - 5;
-                submit.status = 'open';
+                submit.status = 'OPEN';
                 }else if(submit.change[5][1]===diff){
-                submit.change[5][1] = submit.change[5][1] - 5;
-                    submit.status = 'close';
+                    submit.status = 'CLOSED';
                     currencyAvalabilityArray[5][1]=false;
                 }
-            }else if(diff>=1 && currencyAvalabilityArray[4][1]){
-            if(submit.change[4][1]>diff){
-                submit.change[4][1] = submit.change[4][1] - 1;
-                submit.status = 'open';
-                }else if(submit.change[4][1]===diff){
-                submit.change[4][1] = submit.change[4][1] - 1;
-                    submit.status = 'close';
-                    currencyAvalabilityArray[4][1]=false;
-                }
-            }else if(diff>=0.25 && currencyAvalabilityArray[3][1]){
-                if(submit.change[3][1]>diff){
-                    submit.change[3][1] = submit.change[3][1] - 0.25;
-                    submit.status = 'open';
-                    }else if(submit.change[3][1]===diff){
-                    submit.change[3][1] = submit.change[3][1] - 0.25;
-                        submit.status = 'close';
-                        currencyAvalabilityArray[3][1]=false;
-                    }
-            }else if(diff>=0.1 && currencyAvalabilityArray[2][1]){
-                if(submit.change[2][1]>diff){
-                    submit.change[2][1] = submit.change[2][1] - 0.1;
-                    submit.status = 'open';
-                    }else if(submit.change[2][1]===diff){
-                    submit.change[2][1] = submit.change[2][1] - 0.1;
-                        submit.status = 'close';
-                        currencyAvalabilityArray[2][1]=false;
-                    }
-            }else if(diff>=0.05 && currencyAvalabilityArray[1][1]){
-                if(submit.change[1][1]>diff){
-                    submit.change[1][1] = submit.change[1][1] - 0.05;
-                    submit.status = 'open';
-                    }else if(submit.change[1][1]===diff){
-                        submit.change[1][1] = submit.change[1][1] - 0.05;
-                        submit.status = 'close';
-                        currencyAvalabilityArray[1][1]=false;
-                    }
-            }else if(diff>=0.01 && currencyAvalabilityArray[0][1]){
-                if(submit.change[0][1]>diff){
-                    submit.change[0][1] = submit.change[0][1] - 0.01;
-                    submit.status = 'open';
-                    }else if(submit.change[0][1]===diff){
-                        submit.change[0][1] = submit.change[0][1] - 0.01;
-                        submit.status = 'close';
-                        currencyAvalabilityArray[0][1]=false;
-                    }
+                diff = diff - 5;
+                submit.change[5][1] = submit.change[5][1] - 5;
+        }else if(diff>=1 && currencyAvalabilityArray[4][1]){
+        if(submit.change[4][1]>diff){
+            submit.status = 'OPEN';
+            }else if(submit.change[4][1]===diff){    
+                submit.status = 'CLOSED';
+                currencyAvalabilityArray[4][1]=false;
             }
-    } */
-    return 'submit';
+            diff = diff - 1;
+            submit.change[4][1] = submit.change[4][1] - 1;
+        }else if(diff>=0.25 && currencyAvalabilityArray[3][1]){
+            if(submit.change[3][1]>diff){
+                submit.status = 'OPEN';
+                }else if(submit.change[3][1]===diff){
+                    submit.status = 'CLOSED';
+                    currencyAvalabilityArray[3][1]=false;
+                }
+                submit.change[3][1] = submit.change[3][1] - 0.25;
+                diff = diff - 0.25;
+        }else if(diff>=0.1 && currencyAvalabilityArray[2][1]){
+            if(submit.change[2][1]>diff){
+                submit.status = 'OPEN';
+                }else if(submit.change[2][1]===diff){
+                    submit.status = 'CLOSED';
+                    currencyAvalabilityArray[2][1]=false;
+                }
+                submit.change[2][1] = submit.change[2][1] - 0.1;
+                diff = diff - 0.1
+        }else if(diff>=0.05 && currencyAvalabilityArray[1][1]){
+            if(submit.change[1][1]>diff){
+                submit.status = 'OPEN';
+                }else if(submit.change[1][1]===diff){
+                    submit.status = 'CLOSED';
+                    currencyAvalabilityArray[1][1]=false;
+                }
+                submit.change[1][1] = submit.change[1][1] - 0.05;
+                diff = diff - 0.05;
+        }else if(diff>=0.01 && currencyAvalabilityArray[0][1]){
+            if(submit.change[0][1]>diff){
+                submit.status = 'OPEN';
+                }else if(submit.change[0][1]===diff){
+                    submit.status = 'CLOSED';
+                    currencyAvalabilityArray[0][1]=false;
+                }
+                submit.change[0][1] = submit.change[0][1] - 0.01;
+                diff = diff - 0.01; 
+        }
+        console.log(diff)
     }
+    if(diff>0){
+        submit.status = 'INSUFFICIENT_FUNDS'
+    }
+    console.log(submit)
+    return 'submit';
+}
     
-    checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
