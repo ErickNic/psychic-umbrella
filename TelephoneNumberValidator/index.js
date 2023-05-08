afe0404b206775dbae5fd7a4fb0efd60db2b610d
Erick Martin Meza Alonso <erickmartin.mezaalonso200245@gmail.com>
2023-05-08 17:20:59 -0600

    Telephone number validator working perfectly = 5/5 proyects done

---
+++ TelephoneNumberValidator/index.js
@@ -0,0 +1,12 @@
+function telephoneCheck(phoneNumber) {
+    var regex = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+    if (regex.test(phoneNumber)) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+telephoneCheck("555-555-5555");
+console.log(telephoneCheck("11 555-555-5555"))
+
